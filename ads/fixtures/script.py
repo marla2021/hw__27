fa80da5bbeead5115a36547f33ce3401b280b1d4
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-13 21:40:02 +0300

    add commit

--- ads/fixtures/script.py
+++ ads/fixtures/script.py
@@ -4,7 +4,9 @@ import pandas
 
 data_cat=pandas.read_csv("/Users/marinalarutina/PycharmProjects/hw__27/data/categories.csv",sep=",", encoding='utf-8').to_json()
 data_ad=pandas.read_csv("/Users/marinalarutina/PycharmProjects/hw__27/data/ads.csv", sep=",").to_json()
-with open("/Users/marinalarutina/PycharmProjects/hw__27/fixtures/ads/data_c.json", "w", encoding='utf-8') as write_file:
+
+with open("/Users/marinalarutina/PycharmProjects/hw__27/ads/fixtures/data_cat.json", "w", encoding='utf-8') as write_file:
     json.dump(data_cat, write_file,ensure_ascii=False)
 
-print(data_cat)
+with open("/Users/marinalarutina/PycharmProjects/hw__27/ads/fixtures/data_ad.json", "w", encoding='utf-8') as write_file:
+    json.dump(data_ad, write_file,ensure_ascii=False)
