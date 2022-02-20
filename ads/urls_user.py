6751050349fad0fb1fc516eecce1ecfb8aeb8b04
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-20 18:55:52 +0300

    add commit

--- ads/urls_user.py
+++ ads/urls_user.py
@@ -5,7 +5,7 @@ from ads import views
 urlpatterns = [
     path('', views.UserListView.as_view()),
     path('create/', views.UserCreateView.as_view()),
-    # path('<int:pk>', views.CategoryDetailView.as_view()),
-    # path('<int:pk>/update/', views.CategoryUpdateView.as_view()),
-    # path('<int:pk>/delete/', views.CategoryDeleteView.as_view()),
+    path('<int:pk>', views.UserDetailView.as_view()),
+    path('<int:pk>/update/', views.UserUpdateView.as_view()),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view()),
 ]
--- ads/views.py
+++ ads/views.py
@@ -279,3 +279,57 @@ class UserCreateView(CreateView):
             "age": user.is_published,
             "location": user.is_published,
         }, status=status.HTTP_201_CREATED)
+
+class UserDetailView(DetailView):
+    model = User
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_object()
+
+        return JsonResponse({
+            "id": user.id,
+            "first_name": user.name,
+            "last_name": user.author,
+            "username": user.price,
+            "password": user.description,
+            "role": user.address,
+            "age": user.is_published,
+            "location": user.is_published,
+        })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ["first_name", "last_name", "username", "password", "role", "age", "location" ]
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+
+        user_data = json.loads(request.body)
+        self.object.username = user_data["username"]
+        self.object.save()
+
+        return JsonResponse({
+            "id": self.object.id,
+            "first_name": self.object.first_name,
+            "last_name": self.object.last_name,
+            "username":  self.object.username,
+            "password":  self.object.password,
+            "role":  self.object.role,
+            "age":  self.object.age,
+            "location": self.object.location,
+        })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = "/"
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({
+            "status": "ok"
+        }, status=200)
