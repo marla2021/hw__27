7b0b0e43fcefbd6fd489ea3b92f2269aed5316f9
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-20 14:36:50 +0300

    add admin.py

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from ads.models import Ad, Category, Location, User
+
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(User)
